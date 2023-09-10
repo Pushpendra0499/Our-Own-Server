1719c614172113b41f27403a1c067e18ab89883a
Mukku <mukkuraj0499@gmail.com>
2023-09-10 21:23:09 +0530

    Post-request

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ app.listen(8000,() => {
     console.log("My Server started at port No. :8000")
 });
 
-////get request
+////  //////////////// get request /////////////////////
 app.get('/',(req,res) =>{
     res.send("Hello coders! We've just set up our own server")
 })
+
+
+///////////////////// Post Request ////////////////
+//Used tom parse request body in express
+const bodyParser =require('body-parser');
+
+//specially Parse JSON data & add it to the request object
+app.use(bodyParser.json());
+
+
+//// For submit data post request 
+app.post('/api/cars',(req ,res) =>{
+    const{name,brand} =req.body;
+    console.log(name);
+    console.log(brand);
+    res.send("Car Submitted Successfully")
+});
